05c6f6ed2e74bbd2a1ec8178d00b3d3fcaa61b9c
Stanila Kirill <stanila2000@mail.ru>
2026-05-21 11:24:06 +0300

    Changes

--- test/test_archive.py
+++ test/test_archive.py
@@ -1,39 +1,40 @@
 import io
-import os
 import zipfile
 import csv
+
+import pytest
 from openpyxl import load_workbook
 from pypdf import PdfReader
-
-# Прописываем путь для архива
-
-current_dir = os.path.dirname(os.path.abspath(__file__))
-results_dir = os.path.join(current_dir, "results")
-
-archive_path = os.path.join(results_dir, "archive.zip")
 
 
 # Создание архива
 
-with zipfile.ZipFile(archive_path, 'w') as zipf:
-    zipf.write('test/resources/test_file_1.csv')
-    zipf.write('test/resources/test_file_2.pdf')
-    zipf.write('test/resources/test_file_3.xlsx')
+@pytest.fixture
+def archive_path(tmp_path):
+    archive = tmp_path / "test_archive.zip"
+
+    with zipfile.ZipFile(archive, 'w') as zipf:
+        zipf.write('resources/test_file_1.csv')
+        zipf.write('resources/test_file_2.pdf')
+        zipf.write('resources/test_file_3.xlsx')
+
+    return archive
+
 
 # Проверка, что архив создан
 
-def archive_exists(archive_path):
-    assert os.path.isfile(archive_path), f"Архив не найден"
+def test_archive_exists(archive_path):
+    assert archive_path.is_file(), "Архив не найден"
 
 # Проверка количества файлов
 
-def amount_of_files(archive_path):
+def test_amount_of_files(archive_path):
     with zipfile.ZipFile(archive_path, 'r') as zipf:
         assert len(zipf.namelist()) == 3, f"Количество файлов не равно 3"
 
 # Проверка на уникальность
 
-def unic_files(archive_path):
+def test_unic_files(archive_path):
     with zipfile.ZipFile(archive_path, 'r') as zipf:
         seen = set()
         for item in zipf.namelist():
@@ -44,21 +45,21 @@ def unic_files(archive_path):
 
 # Проверка на пустоту и битые файлы
 
-def files_are_not_empty(archive_path):
+def test_files_are_not_empty(archive_path):
     with zipfile.ZipFile(archive_path, 'r') as zipf:
         for filename in zipf.namelist():
             assert len(zipf.read(filename)) > 0, f"Пустой или битый файл {filename}"
 
 # Проверка на типы файлов
 
-def check_files_types(archive_path):
+def test_files_types(archive_path):
     with zipfile.ZipFile(archive_path, 'r') as zipf:
         for filename in zipf.namelist():
             assert filename.endswith((".pdf", ".csv", ".xlsx")), f"Ошибка в файле {filename}"
 
 # Проверка информации в файле pdf
 
-def check_pdf(archive_path):
+def test_pdf(archive_path):
     with zipfile.ZipFile(archive_path, 'r') as zipf:
         for filename in zipf.namelist():
             if filename.endswith(".pdf"):
@@ -69,7 +70,7 @@ def check_pdf(archive_path):
 
 # Проверка информации в файле csv
 
-def check_csv(archive_path):
+def test_csv(archive_path):
     with zipfile.ZipFile(archive_path, 'r') as zipf:
         for filename in zipf.namelist():
             if filename.endswith(".csv"):
@@ -82,7 +83,7 @@ def check_csv(archive_path):
 
 # Проверка информации в файле xlsx
 
-def check_xlsx(archive_path):
+def test_xlsx(archive_path):
     with zipfile.ZipFile(archive_path, 'r') as zipf:
         for filename in zipf.namelist():
             if filename.endswith(".xlsx"):
@@ -90,14 +91,3 @@ def check_xlsx(archive_path):
                     workbook = load_workbook(file)
                     sheet = workbook.active
                     assert sheet.cell(row=2, column=5).value == "Соответствует", "Текст в ячейке xlsx не соответствует"
-
-
-archive_exists(archive_path)
-amount_of_files(archive_path)
-unic_files(archive_path)
-files_are_not_empty(archive_path)
-check_files_types(archive_path)
-check_pdf(archive_path)
-check_csv(archive_path)
-check_xlsx(archive_path)
-
